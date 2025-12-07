8f6911935af75c53ae7854a9fc52ab3aa891d7d7
Ankur Sardar <ankursardar.work@gmail.com>
2025-12-07 18:38:50 +0530

    updates

--- C Programming/Assignment/RTV /9.temp7Days.c
+++ C Programming/Assignment/RTV /9.temp7Days.c
@@ -11,8 +11,7 @@
 
 #include <stdio.h>
 
-// function to calculate average
-float calAvg(int *m, int n)
+float calcuAvg(int *m, int n)
 {
     int sum = 0;
     for (int i = 0; i < n; i++)
@@ -22,7 +21,6 @@ float calAvg(int *m, int n)
     return (float)sum / n;
 }
 
-// function to calculate highest temperature
 float highest(int *m, int n)
 {
     int high = *m;
@@ -36,7 +34,6 @@ float highest(int *m, int n)
     return high;
 }
 
-// function to calculate lowest temperature
 float lowest(int *m, int n)
 {
     int low = *m;
@@ -53,22 +50,19 @@ float lowest(int *m, int n)
 int main()
 {
     int temp[7];
-
-    // taking the temperature input
     printf("\nEnter %d Days Temperature(in °C):\n", 7);
     for (int i = 0; i < 7; i++)
     {
         if(scanf("%d", &temp[i]) !=1 || temp[i]<0 || temp[i]>100){
-            printf("Invalid Temperature (0–100)!\n");
+            printf("Invalid Range (0–100)!\n");
             return 0;
         }
     }
 
-    // printing the temperature
-    printf("\n----- Temperature Analytics -----\n");
+    printf("\n----- Temperature Analytics (7 Days) -----\n");
 
-    printf("\nAverage Temperature : %.2f°C\n",calAvg(temp, 7));
-    printf("Highest Temperature : %.2f°C\n", highest(temp, 7));
-    printf("Lowest Temperature : %.2f°C\n", lowest(temp, 7));
+    printf("\nAverage Temperature (7 Days): %.2f°C\n",calcuAvg(temp, 7));
+    printf("Highest Temperature (7 Days): %.2f°C\n", highest(temp, 7));
+    printf("Lowest Temperature (7 Days): %.2f°C\n", lowest(temp, 7));
     return 0;
 }
